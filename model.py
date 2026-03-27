ce71687f1ab8ff4f047f63582963fbf5bf38a913
Gia99999 <55598854@qq.com>
2026-03-27 09:14:43 +0000

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,78 @@
+class CNNMnist(nn.Module):
+    def __init__(self, args):
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, args.num_classes)
+        
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
+      
+class CNNCifar10(nn.Module):
+    def __init__(self, args):
+        super(CNNCifar10, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(args.num_channels, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 4 * 4, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, args.num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+  class ResNet18Cifar100(nn.Module):
+    def __init__(self, args):
+        super(ResNet18Cifar100, self).__init__()
+        self.resnet = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+        self.resnet.conv1 = nn.Conv2d(args.num_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.resnet.maxpool = nn.Identity()
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs * 2),         
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),                
+            nn.Linear(num_ftrs * 2, args.num_classes)
+        )
+
+    def forward(self, x):
+        return self.resnet(x)
